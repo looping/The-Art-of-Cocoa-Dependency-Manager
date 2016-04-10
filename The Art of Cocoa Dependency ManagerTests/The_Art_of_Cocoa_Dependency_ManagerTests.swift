415435fed4df5c80279e526c7fc84297db062582
Looping <looping1989@163.com>
2016-04-10 14:31:21 +0800

    Add project

---
+++ The Art of Cocoa Dependency ManagerTests/The_Art_of_Cocoa_Dependency_ManagerTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_Art_of_Cocoa_Dependency_ManagerTests.swift
+//  The Art of Cocoa Dependency ManagerTests
+//
+//  Created by Looping on 4/10/16.
+//  Copyright © 2016 RidgeCorn. All rights reserved.
+//
+
+import XCTest
+@testable import The_Art_of_Cocoa_Dependency_Manager
+
+class The_Art_of_Cocoa_Dependency_ManagerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
