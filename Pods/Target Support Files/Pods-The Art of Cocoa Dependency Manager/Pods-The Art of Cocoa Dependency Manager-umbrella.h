5bcd01f089d63caa4326b2198242d54b832e77b9
Looping <looping1989@163.com>
2016-04-10 22:52:32 +0800

    Using Carthage

--- Pods/Target Support Files/Pods-The Art of Cocoa Dependency Manager/Pods-The Art of Cocoa Dependency Manager-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_The_Art_of_Cocoa_Dependency_ManagerVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_The_Art_of_Cocoa_Dependency_ManagerVersionString[];
-
